853242365dc70323fe0e11f364e44d45e10be800
Gabriel <gabe.cs1997@gmail.com>
2022-03-11 01:57:20 -0500

    adding more features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
+
+  const [data,setData] = useState({})
+  const [location, setLocation] = useState('')
+
+
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=085c3e3f13130f3f56e53aa88093eeeb`
+
+
+ const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+
+      setLocation('')
+    }
+  }
+
+
+
+  return ( 
+    <div className="app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange= {event => setLocation(event.target.value)}
+        placeholder='Enter Location'
+        onKeyPress={searchLocation}
+        type="text" />
+      </div>
+      <div className="container">
+        <div className="top">
+        <div className="location">
+          <p>{data.name}</p>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp} ℉</h1> : null}
+          </div>
+        </div> 
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main} </p> : null}
+          </div>
+        </div>
+
+{data.name != undefined && 
+        <div className="bottom">
+        <div className="feels">
+          {data.main ? <p className="bold">{data.main.feels_like} ℉</p> : null}
+          <p className="">feels like</p>
+        </div>
+        <div className="humidity">
+        {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+          <p>Humidity</p>
+        </div>
+        <div className="wind">
+        {data.wind ? <p className="bold">{data.wind.speed} Mph</p> : null}
+          <p>Wind Speed</p>
+        </div>
+      </div>
+}
+
+      </div>
+
     </div>
   );
 }
